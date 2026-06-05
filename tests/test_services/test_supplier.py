0994e31d0df2a8430d1c02e0649387a0b936b032
arvinnick <arvin.nick.90@gmail.com>
2026-06-06 00:10:23 +0400

    All tests passed

--- tests/test_services/test_supplier.py
+++ tests/test_services/test_supplier.py
@@ -14,4 +14,4 @@ async def test_retrieve_suppliers_for_ingredient_zero(db_instance):
 async def test_retrieve_suppliers_for_ingredient_one(db_instance):
     ingredient = "White Sugar"
     result = await retrieve_suppliers_for_ingredient(ingredient, db_instance)
-    assert [supp.name for supp in result] == ["Sepehr Machinery"]
+    assert [supp.name for supp in result] == ["Tehran Supply Co."]
